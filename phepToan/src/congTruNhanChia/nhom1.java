ba389e39cc18c785726ce77b267d4b36f60e3776
HoangPhuocNhan <hoangphuocnhan99@gmail.com>
2021-07-28 13:15:25 +0700

    add new project

--- phepToan/src/congTruNhanChia/nhom1.java
+++ phepToan/src/congTruNhanChia/nhom1.java
@@ -3,6 +3,14 @@ package congTruNhanChia;
 
 public class nhom1 {
 	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+         
+        System.out.println("Nhập vào số thứ nhất: ");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Nhập vào số thứ hai: ");
+        int secondNumber = scanner.nextInt();
+        
         //viết code ở đây nhé mấy bạn
     }
 
